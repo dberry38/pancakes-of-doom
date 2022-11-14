710f536c9efa7e74b00f8d84903e79e49aea3bf8
dberry38 <108836058+dberry38@users.noreply.github.com>
2022-11-14 14:56:48 -0800

    Developing index.js in the models folder

--- models/index.js
+++ models/index.js
@@ -4,13 +4,26 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category - done
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+// Categories have many Products - done
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag) - done
+Product.belongsToMany(Tag, {
+  foreignKey: 'tag_id',
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag) - done
+Tag.belongsToMany(Product, {
+  foreignKey: 'product_id',
+});
 
 module.exports = {
   Product,
